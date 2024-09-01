c56958fc4af18e873477b9088d8cb11d76f6a3a6
hadiatoubalde <hadiatou.balde025@gmail.com>
2024-09-01 16:02:23 +0200

    Ajout des event listeners, Ajout des bullets points au slider, Mise à jour des bullets, Modifier le slide au click du button, Mettre en place le défilement infini sur le carrousel

--- assets/script.js
+++ assets/script.js
@@ -17,12 +17,60 @@ const slides = [
 	}
 ]
 
-//Ajout des event listener
-const arrows = document.querySelectorAll(".arrow");
 
-arrows.forEach((arrow) => {
-	arrow.addEventListener('click', (e) => {
-		console.log(e.target.id);
-	})
-})
+document.addEventListener('DOMContentLoaded', function() {
+	let currentSlide = 0;
+	const bannerImg = document.querySelector('.banner-img');
+	const dots = document.querySelectorAll('.dot');
+
+	//Ajout des event listener
+	function showSlide(index) {
+		currentSlide = index;
+
+		if(bannerImg) {
+			bannerImg.src = `./assets/images/slideshow/${slides[index].image}`;
+
+			//Mettre à jour les points actifs
+			dots.forEach(dot => dot.classList.remove('active'));
+			if(dots[index]) {
+				dots[index].classList.add('active');
+			}
+		}else {
+			console.error("Error: L'image du slider n'a pas été trouvée.");
+		}
+	}
+
+	function nextSlide() {
+		currentSlide = (currentSlide + 1) % slides.length;
+		showSlide(currentSlide);
+	}
+
+	function prevSlide() {
+		currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+		showSlide(currentSlide);
+	}
+
+	const arrowLeft = document.querySelector('.arrow_left');
+	const arrowRight = document.querySelector('.arrow_right');
+
+	if(arrowLeft && arrowRight) {
+		arrowLeft.addEventListener('click', function() {
+			prevSlide();
+		});
+
+		arrowRight.addEventListener('click', function() {
+			nextSlide();
+		});
+	}else {
+		console.error("Error : Les flèches n'ont pas été trouvées");
+	}
+
+	//Initialisation de la première slide
+	showSlide(0);
+
+	//Ajout d'un interval automatique pour changer la slide toutes les 5s
+	setInterval(nextSlide, 5000);
+});
+
+
 
